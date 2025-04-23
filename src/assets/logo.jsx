e8c68730281f6874d28eaca228db092e240ab142
LocoIcee <62861210+LocoIcee@users.noreply.github.com>
2025-04-22 21:26:22 -0600

    update header logo

--- src/assets/logo.jsx
+++ src/assets/logo.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 export const Logo = ({ className }) => {
   return (
-    <div className={`${className} overflow-hidden rounded-full bg-white shadow-md relative p-0`}>
+    <div className={`${className} overflow-hidden rounded-full bg-white shadow-md relative`}>
       <img 
         src="/assets/images/logo-black.jpg" 
         alt="Almost Home Canine Rescue Logo" 
-        className="absolute inset-0 w-[103%] h-[103%] object-cover transform-gpu"
-        style={{ objectPosition: 'center 47%', left: '-1.5%', top: '-1.5%' }}
+        className="absolute inset-0 w-full h-full object-cover transform-gpu"
+        style={{ objectPosition: 'center' }}
       />
     </div>
   );
