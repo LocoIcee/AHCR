749e2fdf8bb3c2577590c20a92276e7b67d12d72
Cam <cameron13@live.ca>
2025-09-06 13:23:02 -0600

    remove extras

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,12 +1,7 @@
-import Script from "next/script";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-
-import { Providers } from './providers';
-import { PaymentModalProvider } from '@/context/PaymentModalContext';
-import GlobalPayments from '@/components/GlobalPayments';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,15 +22,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <Providers>
-          <PaymentModalProvider>
-            <Header />
-            {children}
-            <GlobalPayments />
-            <Footer />
-          </PaymentModalProvider>
-        </Providers>
-        <Script src="https://www.paypal.com/sdk/js?client-id=AUd6J6g-t_SidUfE-VzzWYFPfGP3sCgQ3GsY6B_3SytmRX-OawWqQCwDA8f13KIqHeNTafswnIR1vLI8&currency=CAD" strategy="afterInteractive" />
+        <Header />
+        {children}
+        <Footer />
       </body>
     </html>
   )
