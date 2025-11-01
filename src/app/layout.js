bcbc00dda70e343bcd1a145b94fa30550adef2f6
Cam <cameron13@live.ca>
2025-11-01 12:23:27 -0600

    remove speed insights

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
